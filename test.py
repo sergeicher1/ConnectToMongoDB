bdffd5056b2e1db55faf396a072051c3f990249f
sergei <sergeicher87@gmail.com>
2024-02-03 10:44:31 +0200

    Cute Number question

--- test.py
+++ test.py
@@ -9,53 +9,99 @@
 # -- Version                                  | 3.12
 # -- Description                              | Exercise "cute_numbers"
 # Usage Example
-# Just run the file to execute the script
+# Uncomment unit testt and just run the file to execute the script
+# The Question:
+# נתונות הגדרות הבאות:
+# • מספר שלם חיובי נקרא "חמוד" אם הספרה הימנית שלו היא כפולה של הספרה השמאלית שלו
+# לדוגמה:
+# המספרים 2378 ,737 ,16, 48 הם מספרים "חמודים" כי בכל אחד מהמספרים האלה הספרה הימנית ביותר מתחלקת בספרה השמאלית ביותר ללא שארית.
+# • עמודה במערך דו -ממדי נקראת "נחמדה" אם כל הערכים הנמצאים בעמודה זו הם "חמודים".
+# • מערך דו-ממדי נקרא "מותק" אם יש בו לפחות עמודה אחת שהיא "נחמדה".
+# א. כתוב פעולה אשר מקבלת מערך דו-ממדי ומחזירה "אמת" אם הוא "מותק" או "שקר " אם הוא אחר.
 # ------------------------------------------------------------------------------------------------
 
 
 """Function to check if the given number is "cute\""""
-arr = [
-    [12, 24, 26],
-    [39, 48, 36]
+arr = [12, 24, 26]
+wrong_arr = [23, 27, 35]
+doubled_arr = [
+    [36, 24, 26],
+    [48, 28, 39]
+]
+wrong_doubled_arr = [
+    [37, 25, 27],
+    [45, 23, 29]
 ]
 
 
 def CuteNumber(number):
     # Type cast the given number to a string
     number_to_string = str(number)
-    return int(number[-1]) % int(number[0]) == 0
+    # print(len(number_to_string))
+    # print(int(number_to_string[-1]) % int(number_to_string[0]))
+    return "The num is cute" if len(number_to_string) > 1 and int(number_to_string[-1]) % int(
+        number_to_string[0]) == 0 else False
 
 
-def CheckArray(doubled_list):
-    # number_of_items = len(doubled_list)
-    doubled_list = arr
-    number_of_items = 2
-    if CuteNumber(doubled_list[0][0]):
-        if CuteNumber(doubled_list[1][0]):
-            if CuteNumber(doubled_list[0][1]):
-                if CuteNumber(doubled_list[1][1]):
-                    return "ok"
-    return doubled_list
+# Unit tests
+# print(CuteNumber(24))
+# print(CuteNumber(27))
+# print(CuteNumber(2))
 
-# def cute_arr(arr, column):
-#     for i in arr:
-#         if cute_num(i[column]) == False:
-#             return False
-#     return True
-#
-#
-# def honey_arr(arr):
-#     for i in range(len(arr[0])):
-#         if cute_arr(arr, i) == True:
-#             return True
-#     return False
+"""Function to check the given array if it is cute"""
 
 
-# arr1 = [[12, 24, 26], [23, 23, 23], [23, 23, 23]]
-# arr2 = [[23, 24, 23], [23, 48, 23], [23, 17, 23]]
-# arr3 = [[1, 3, 5], [6, 7, 8], [9, 3, 2]]
+def CuteArray(regular_array):
+    for i in regular_array:
+        if CuteNumber(i):
+            continue
+        else:
+            print(i)
 
-# print(f"{honey_arr(arr)} need to be => True.")
-# print(f"{honey_arr(arr1)} need to be => False.")
-# print(f"{honey_arr(arr2)} need to be => True.")
-# print(f"{honey_arr(arr3)} need to be => True.")
+
+# Unit tests
+# CuteArray(arr)
+# CuteArray(wrong_arr)
+
+
+"""Function to check the given doubled_array if it is cute"""
+
+
+def CuteDoubledArray(dub_array):
+    for i in dub_array:
+        for j in i:
+            if CuteNumber(j):
+                continue
+            else:
+                print(j)
+
+
+# Unit tests
+# CuteDoubledArray(doubled_arr)
+# CuteDoubledArray(wrong_doubled_arr)
+
+"""Function to check the column in the given doubled_array if it is cute"""
+
+
+# FIXME: Implement this function to check columns and then to use in CheckSweetArray
+def CheckColumn(dub_array):
+    for i in range(len(dub_array[0])):
+        if CuteNumber(dub_array[0][i]) and CuteNumber(dub_array[1][i]):
+            # print("Column is 'fine'")
+            continue
+        else:
+            return False
+            # print("Column is not 'fine'")
+
+
+# Unit tests
+CheckColumn(dub_array=doubled_arr)
+CheckColumn(dub_array=wrong_doubled_arr)
+
+"""Function to check the column in the given doubled_array if it is cute"""
+
+
+# TODO: Implement this function
+def CheckSweetArray(dub_array):
+    sweet = 0
+    pass
---
+++ test1.py
@@ -0,0 +1,21 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11	<! DOCTYPE html>
+<html>
+<head>
+ <meta charset="utf-8" />
+<title>Django on zero-to-hero.dev</title>
+</head>
+<body>
+<h2>Home</h2>
+{% include "banner.html" with tutorial="Python" %}
+</body>
+</html>
